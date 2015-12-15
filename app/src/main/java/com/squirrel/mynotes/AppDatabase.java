4abaa2d4ff73a48984f290cc7636d680c2be9ae8
tisqui <squiiirrel@gmail.com>
2015-12-14 22:25:13 -0800

    Added database classes

---
+++ app/src/main/java/com/squirrel/mynotes/AppDatabase.java
@@ -0,0 +1,66 @@
+package com.squirrel.mynotes;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+/**
+ * Created by squirrel on 12/14/15.
+ */
+public class AppDatabase extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "mynotes.db";
+    private static final int DATABASE_VERSION = 1;
+
+    interface Tables {
+        String Notes = "notes";
+        String Archives = "archives";
+        String Deleted = "deleted";
+    }
+
+    public AppDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //create Notes table
+        db.execSQL("CREATE TABLE " + Tables.Notes + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + NotesContract.NotesColumns.NOTE_TITLE + " TEXT NOT NULL,"
+                + NotesContract.NotesColumns.NOTE_DESCRIPTION + " TEXT NOT NULL,"
+                + NotesContract.NotesColumns.NOTE_TIME + " TEXT NOT NULL,"
+                + NotesContract.NotesColumns.NOTE_TYPE + " TEXT NOT NULL,"
+                + NotesContract.NotesColumns.NOTE_IMAGE_STORAGE_SELECTION + " INTEGER NOT NULL,"
+                + NotesContract.NotesColumns.NOTE_IMAGE + " TEXT NOT NULL,"
+                + NotesContract.NotesColumns.NOTE_DATE + " TEXT NOT NULL)");
+        //create Archives table
+        db.execSQL("CREATE TABLE " + Tables.Archives + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + ArchivesContract.ArchivesColumns.ARCHIVES_TITLE + " TEXT NOT NULL,"
+                + ArchivesContract.ArchivesColumns.ARCHIVES_DESCRIPTION + " TEXT NOT NULL,"
+                + ArchivesContract.ArchivesColumns.ARCHIVES_CATEGORY + " TEXT NOT NULL,"
+                + ArchivesContract.ArchivesColumns.ARCHIVES_TYPE + " TEXT NOT NULL,"
+                + ArchivesContract.ArchivesColumns.ARCHIVES_DATE_TIME+ " TEXT NOT NULL)");
+        //create Deleted table
+        db.execSQL("CREATE TABLE " + Tables.Deleted + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + TrashContract.DeletedColumns.DELETED_TITLE + " TEXT NOT NULL,"
+                + TrashContract.DeletedColumns.DELETED_DESCRIPTION + " TEXT NOT NULL,"
+                + TrashContract.DeletedColumns.DELETED_DATE_TIME+ " TEXT NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void deleteDatabase(Context context){
+        context.deleteDatabase(DATABASE_NAME);
+    }
+
+    public void emptyTrash(){
+        //delete the Deleted table
+        getWritableDatabase().delete(Tables.Deleted, null,null);
+    }
+}
---
+++ app/src/main/java/com/squirrel/mynotes/TrashContract.java
@@ -0,0 +1,36 @@
+package com.squirrel.mynotes;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by squirrel on 12/14/15.
+ */
+public class TrashContract {
+    interface DeletedColumns {
+        String DELETED_ID = "_ID";
+        String DELETED_TITLE = "deleted_title";
+        String DELETED_DESCRIPTION = "deleted_description";
+        String DELETED_DATE_TIME = "deleted_date_time";
+    }
+
+    public static final String CONTENT_AUTHORITY = "com.squirrel.mynotes.provider";
+    public static final Uri URI_BASE = Uri.parse("content://" + CONTENT_AUTHORITY);
+    private static final String PATH_DELETED = "trash";
+    public static final Uri URI_TABLE = URI_BASE.buildUpon().appendEncodedPath(PATH_DELETED).build();
+
+
+    public static class Deleted implements DeletedColumns, BaseColumns {
+
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd." + CONTENT_AUTHORITY + ".trash";
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd." + CONTENT_AUTHORITY + ".trash";
+
+        public static Uri buildDeletedUri(String deletedId) {
+            return URI_TABLE.buildUpon().appendEncodedPath(deletedId).build();
+        }
+
+        public static String getDeletedId(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+    }
+}
